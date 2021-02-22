7a46b951bc2324f0287dc40f511fedb64c30c64e
nesibecetin <nesibe3cetin@hotmail.com>
2021-02-22 17:10:04 +0300

    Added Autofac

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -30,10 +30,10 @@ namespace WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddSingleton<ICarService, CarManager>();
-            services.AddSingleton<ICarDal, EfCarDal>();
-            services.AddSingleton<IBrandService, BrandManager>();
-            services.AddSingleton<IBrandDal, EfBrandDal>();
+            //services.AddSingleton<ICarService, CarManager>();
+            //services.AddSingleton<ICarDal, EfCarDal>();
+            //services.AddSingleton<IBrandService, BrandManager>();
+            //services.AddSingleton<IBrandDal, EfBrandDal>();
 
         }
 
