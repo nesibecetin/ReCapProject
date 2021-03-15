38934c59d03ecaeff605a4338e30db680d1c6553
nesibecetin <nesibe3cetin@hotmail.com>
2021-03-15 12:14:55 +0300

    Added Cache, Transaction and Performance Aspect to Core Layer

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -3,6 +3,9 @@ using Business.BusinessAspects.Autofac;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
 using Core.Aspect.Autofac.Validation;
+using Core.Aspect.Caching;
+using Core.Aspect.Performance;
+using Core.Aspect.Transaction;
 using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
@@ -11,6 +14,7 @@ using Entities.Dtos;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace Business.Concrete
 {
@@ -23,6 +27,7 @@ namespace Business.Concrete
         }
         [SecuredOperation("car.add,admin")]
         [ValidationAspect(typeof(CarValidator))]
+        [CacheRemoveAspect("ICarService.Get")]
         public IResult Add(Car car)
         {
             if(car.DailyPrice>0){
@@ -35,20 +40,23 @@ namespace Business.Concrete
                 return new ErrorResult(Messages.CarInvalid);
         }
 
+       
+
         public IResult Delete(Car car)
         {
             ValidationTool.Validate(new CarValidator(),car);
             _carDal.Delete(car);
             return new SuccessResult(Messages.CarDeleted);
         }
-
+        [PerformanceAspect(5)]
+        [CacheAspect(duration:20)]
         public IDataResult<List<Car>> GetAll()
         {
-          
+            Thread.Sleep(5000);
             return new SuccessDataResult<List<Car>>(_carDal.GetAll(),Messages.CarListed);
 
         }
-
+        [CacheAspect]
         public IDataResult<List<Car>> GetAllById(int id)
         {
             return new SuccessDataResult<List<Car>>(_carDal.GetAll(c => c.CarId == id),Messages.CarListed);
@@ -68,10 +76,17 @@ namespace Business.Concrete
         {
             return new SuccessDataResult<List<Car>>(_carDal.GetAll(c => c.ColorId == id), Messages.CarListed);
         }
-
+        [CacheRemoveAspect("ICarService.Get")]
         public IResult Update(Car car)
         {
             _carDal.Update(car);
+            return new SuccessResult(Messages.CarUpdated);
+        }
+        [TransactionScopeAspect]
+        public IResult AddTransactionalTest(Car car)
+        {
+            _carDal.Update(car);
+            _carDal.Add(car);
             return new SuccessResult(Messages.CarUpdated);
         }
     }
--- Core/DependencyResolvers/CoreModule.cs
+++ Core/DependencyResolvers/CoreModule.cs
@@ -1,8 +1,11 @@
-﻿using Core.Utilities.IOC;
+﻿using Core.CrossCuttingConcerns.Caching;
+using Core.CrossCuttingConcerns.Caching.Microsoft;
+using Core.Utilities.IOC;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 
 namespace Core.DependencyResolvers
@@ -13,7 +16,8 @@ namespace Core.DependencyResolvers
         {
             serrviceCollection.AddMemoryCache();
             serrviceCollection.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-            //serrviceCollection.AddSingleton<ICacheManager, MemoryCacheManager>();
+            serrviceCollection.AddSingleton<ICacheManager, MemoryCacheManager>();
+            serrviceCollection.AddSingleton<Stopwatch>();
         }
     }
 }
